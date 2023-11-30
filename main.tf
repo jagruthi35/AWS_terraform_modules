c198938c88d4256086f640d46596bdff2b9a7f0c
jagruthi35 <114712771+jagruthi35@users.noreply.github.com>
2023-11-30 10:47:53 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "cpeibucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_object" "object" {
+  bucket = aws_s3_bucket.example.bucket
+  key    = "organizations-100.csv"
+  source = "./organizations-100.csv"
+}
